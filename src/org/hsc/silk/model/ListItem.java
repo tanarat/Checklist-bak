a1dbf0aacde69d222984dc52ac859204cf8a14cd
Tanarat Thanissa <th.tanarat@gmail.com>
2012-10-13 15:32:39 +0700

    chang many things

--- src/org/hsc/silk/model/ListItem.java
+++
@@ -1,8 +0,0 @@
-package org.hsc.silk.model;
-
-public interface ListItem {
-	public long getItemId();
-	public String getItemName();
-	public String getItemDescription();
-	public String getItemDescription2();
-}
